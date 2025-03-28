dd0e0fcd8b17b36af2ec70bec109c0dc3b931ced
Luth <71706257+mkluth@users.noreply.github.com>
2025-03-28 15:41:49 +0800

    Bringing back navigation keys support
    Yes, you can now move the cursor and navigate within the currently
    opened file similarly as before.
    
    Signed-off-by: Luth <71706257+mkluth@users.noreply.github.com>

--- src/input.c
+++ src/input.c
@@ -6,6 +6,105 @@
 
 #include <void.h>
 
+static void snap_cur_eol(struct v_state *v)
+{
+	struct v_row *row = (v->cur_y >= v->nrows) ? NULL : &v->rows[v->cur_y];
+	int len = row ? row->len : 0;
+	if (v->cur_x > len)
+		v->cur_x = len;
+}
+
+static void v_ppage(struct v_state *v)
+{
+	v->cur_y = v->rowoff;
+	int times = v->scr_y;
+
+	while (times--) {
+		if (v->cur_y != 0)
+			v->cur_y--;
+		snap_cur_eol(v);
+	}
+}
+
+static void v_npage(struct v_state *v)
+{
+	v->cur_y = v->rowoff + v->scr_y - 1;
+	if (v->cur_y > v->nrows)
+		v->cur_y = v->nrows;
+
+	int times = v->scr_y;
+	while (times--) {
+		if (v->cur_y < v->nrows)
+			v->cur_y++;
+		snap_cur_eol(v);
+	}
+}
+
+static int v_nav(struct v_state *v, int key)
+{
+	struct v_row *row = (v->cur_y >= v->nrows) ? NULL : &v->rows[v->cur_y];
+
+	switch (key) {
+	case V_CUR_LEFT:
+	case KEY_LEFT:
+		/* Cursor left */
+		if (v->cur_x != 0) {
+			v->cur_x--;
+		} else if (v->cur_y > 0) {
+			v->cur_y--;
+			v->cur_x = v->rows[v->cur_y].len;
+		}
+		break;
+	case V_CUR_RGHT:
+	case KEY_RIGHT:
+		/* Cursor right */
+		if (row && v->cur_x < row->len) {
+			v->cur_x++;
+		} else if (row && v->cur_x == row->len) {
+			v->cur_y++;
+			v->cur_x = 0;
+		}
+		break;
+	case V_CUR_UP:
+	case KEY_UP:
+		/* Cursor up */
+		if (v->cur_y != 0)
+			v->cur_y--;
+		break;
+	case V_CUR_DOWN:
+	case KEY_DOWN:
+		/* Cursor down */
+		if (v->cur_y < v->nrows)
+			v->cur_y++;
+		break;
+	case V_CUR_SOL:
+	case KEY_HOME:
+		/* Jump to the start of the line */
+		v->cur_x = 0;
+		return V_OK;
+	case V_CUR_EOL:
+	case KEY_END:
+		/* Jump to the end of the line */
+		if (v->cur_y < v->nrows)
+			v->cur_x = row->len;
+		return V_OK;
+	case KEY_PPAGE:
+		/* Page up */
+		v_ppage(v);
+		return V_OK;
+	case KEY_NPAGE:
+		/* Page down */
+		v_npage(v);
+		return V_OK;
+	default:
+		return V_ERR;
+	}
+
+	snap_cur_eol(v);
+
+	return V_OK;
+}
+
 static int v_quit(struct v_state *v)
 {
 	if (!v->dirty)
@@ -14,7 +113,7 @@ static int v_quit(struct v_state *v)
 	v_set_stats_msg(v, "WARNING: Unsaved changes. Press again to quit.");
 	v_rfsh_scr(v);
 	int key = getch();
-	if (key != V_KEY_QUIT)
+	if (key != V_CTRL_QUIT)
 		return V_ERR;
 
 quit:
@@ -23,8 +122,8 @@ quit:
 }
 
 static const struct v_keybind ctrls[] = {
-	{V_KEY_QUIT, v_quit},
-	{0, NULL}
+	{V_CTRL_QUIT, v_quit},		/* Quit the editor */
+	{0, NULL}			/* Sentinel */
 };
 
 static int v_ctrl(struct v_state *v, int key)
@@ -52,7 +151,13 @@ int v_prcs_key(struct v_state *v)
 		return V_ERR;
 
 	int key = getch();
-	return v_ctrl(v, key);
+	if (v_nav(v, key) == V_OK)
+		return V_OK;
+
+	if (v_ctrl(v, key) == V_OK)
+		return V_OK;
+
+	return V_ERR;
 }
 
 static int get_prompt_input(char *buf, size_t *bufsz, size_t *buflen)
